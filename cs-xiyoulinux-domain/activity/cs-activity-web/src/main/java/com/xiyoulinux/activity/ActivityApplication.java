536a899ca0cb8e0438a4718bab66ee76f7169984
QKM-0203 <com_qkm@xiyoulinux.org>
2021-10-13 19:47:07 +0800

    feat(activity):add web api

---
+++ cs-xiyoulinux-domain/activity/cs-activity-web/src/main/java/com/xiyoulinux/activity/ActivityApplication.java
@@ -0,0 +1,22 @@
+package com.xiyoulinux.activity;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author qkm
+ */
+@SpringBootApplication(scanBasePackages = {"com.xiyoulinux"})
+@MapperScan(basePackages = "com.xiyoulinux.activity.mapper")
+@EnableDiscoveryClient
+@EnableSwagger2
+@EnableHystrix
+public class ActivityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityApplication.class, args);
+    }
+}
