3caead9345df5f8e571845699329e8e39feb6345
QKM-0203 <com_qkm@xiyoulinux.org>
2021-10-15 02:51:13 +0800

    feat(gateway):add dynamic routing(on nacos)

---
+++ cs-xiyoulinux-platform/gateway/src/main/java/com/xiyoulinux/config/DynamicRouteServiceImplByNacos.java
@@ -0,0 +1,132 @@
+package com.xiyoulinux.config;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.common.utils.CollectionUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
+/**
+ * <h1>通过 nacos 下发动态路由配置, 监听 Nacos 中路由配置变更</h1>
+ * @author qkm
+ */
+@Slf4j
+@Component
+@DependsOn({"gateWayConfig"})
+public class DynamicRouteServiceImplByNacos {
+
+    /**
+     * nacos 配置服务
+     */
+    private ConfigService configService;
+    private final DynamicRouteServiceImpl dynamicRouteService;
+
+    public DynamicRouteServiceImplByNacos(DynamicRouteServiceImpl dynamicRouteService) {
+        this.dynamicRouteService = dynamicRouteService;
+    }
+
+    /**
+     * 注解的作用是：当前 Bean 在容器中构造完成之后会立即执行 init 方法
+     */
+    @PostConstruct
+    public void init() {
+
+        log.info("gateway route init....");
+
+        try {
+            // 初始化 Nacos 配置客户端
+            configService = initConfigService();
+            if (null == configService) {
+                log.error("init config service fail");
+                return;
+            }
+
+            // 通过 Nacos Config 并指定路由配置路径去获取路由配置
+            String configInfo = configService.getConfig(
+                    GateWayConfig.NACOS_ROUTE_DATA_ID,
+                    GateWayConfig.NACOS_ROUTE_GROUP,
+                    GateWayConfig.DEFAULT_TIMEOUT
+            );
+
+            log.info("get current gateway config: [{}]", configInfo);
+            List<RouteDefinition> definitionList =
+                    JSON.parseArray(configInfo, RouteDefinition.class);
+
+            if (CollectionUtils.isNotEmpty(definitionList)) {
+                for (RouteDefinition definition : definitionList) {
+                    log.info("init gateway config: [{}]", definition.toString());
+                    dynamicRouteService.addRouteDefinition(definition);
+                }
+            }
+
+        } catch (Exception ex) {
+            log.error("gateway route init has some error: [{}]", ex.getMessage(), ex);
+        }
+
+        // 设置监听器
+        dynamicRouteByNacosListener(GateWayConfig.NACOS_ROUTE_DATA_ID,
+                GateWayConfig.NACOS_ROUTE_GROUP);
+    }
+
+    /**
+     * 初始化 Nacos Config
+     */
+    private ConfigService initConfigService() {
+
+        try {
+            Properties properties = new Properties();
+            properties.setProperty("serverAddr", GateWayConfig.NACOS_SERVER_ADDR);
+            properties.setProperty("namespace", GateWayConfig.NACOS_NAMESPACE);
+            return configService = NacosFactory.createConfigService(properties);
+        } catch (Exception ex) {
+            log.error("init gateway nacos config error: [{}]", ex.getMessage(), ex);
+            return null;
+        }
+    }
+
+    /**
+     * 监听 Nacos 下发的动态路由配置
+     */
+    private void dynamicRouteByNacosListener(String dataId, String group) {
+
+        try {
+            // 给 Nacos Config 客户端增加一个监听器
+            configService.addListener(dataId, group, new Listener() {
+
+                /**
+                 * 自己提供线程池执行操作
+                 * */
+                @Override
+                public Executor getExecutor() {
+                    return null;
+                }
+
+                /**
+                 * 监听器收到配置更新
+                 * @param configInfo Nacos 中最新的配置定义
+                 * */
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+
+                    log.info("start to update config: [{}]", configInfo);
+                    List<RouteDefinition> definitionList =
+                            JSON.parseArray(configInfo, RouteDefinition.class);
+                    log.info("update route: [{}]", definitionList.toString());
+                    dynamicRouteService.updateList(definitionList);
+                }
+            });
+        } catch (NacosException ex) {
+            log.error("dynamic update gateway config error: [{}]", ex.getMessage(), ex);
+        }
+    }
+}
