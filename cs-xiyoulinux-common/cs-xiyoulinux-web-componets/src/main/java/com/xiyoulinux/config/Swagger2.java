d07070ea8ebca14b0212cb58ed24d1a65331c688
QKM-0203 <com_qkm@xiyoulinux.org>
2021-10-15 02:49:31 +0800

    modified swagger version

--- cs-xiyoulinux-common/cs-xiyoulinux-web-componets/src/main/java/com/xiyoulinux/config/Swagger2.java
+++ cs-xiyoulinux-common/cs-xiyoulinux-web-componets/src/main/java/com/xiyoulinux/config/Swagger2.java
@@ -1,8 +1,8 @@
 package com.xiyoulinux.config;
 
+import com.xiyoulinux.annotation.IgnoreResponseAdvice;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -22,20 +22,14 @@ public class Swagger2 {
     // http://localhost:8088/doc.html               引入的依赖皮肤路径
 
 
-    // 配置swagger2核心配置 docket
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)  // 指定api类型为swagger2
                 .apiInfo(apiInfo())                     // 用于定义api文档汇总信息
-                .select()
-                .apis(RequestHandlerSelectors
-                        .withClassAnnotation(RestController.class) // 指定Service上的RestController注解
-//                        .basePackage("com.zk.controller")) // 指定controller包
-                )
+                .select().apis(RequestHandlerSelectors.basePackage("com.xiyoulinux.activity")) // 指定controller包
                 .paths(PathSelectors.any())             // 包下的所有controller
                 .build();
     }
-
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("CS平台接口api")           // 文档页标题
