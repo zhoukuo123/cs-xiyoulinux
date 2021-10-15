7e075e290a1156657524430c93e4f7bf97972a6e
QKM-0203 <com_qkm@xiyoulinux.org>
2021-10-15 19:54:26 +0800

    feat(activity):add static string variable

---
+++ cs-xiyoulinux-domain/activity/cs-activity-service/src/main/java/com/xiyoulinux/activity/constant/ActivityConstant.java
@@ -0,0 +1,10 @@
+package com.xiyoulinux.activity.constant;
+
+/**
+ * @author qkm
+ */
+public class ActivityConstant {
+    public static String PAGE_TASK_DOING = "CS:TASK:DOING:";
+    public static String PAGE_TASK_DID = "CS:TASK:DID:";
+    public static String PAGE_TASK_FUTURE = "CS:TASK:FUTURE:";
+}
