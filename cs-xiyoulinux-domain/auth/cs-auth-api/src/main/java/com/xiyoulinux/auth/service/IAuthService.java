40abb81d2e473867310fcd0b26b7efd73eda11a1
QKM-0203 <com_qkm@xiyoulinux.org>
2021-10-17 19:58:52 +0800

    feat(auth):add login and register api

---
+++ cs-xiyoulinux-domain/auth/cs-auth-api/src/main/java/com/xiyoulinux/auth/service/IAuthService.java
@@ -0,0 +1,25 @@
+package com.xiyoulinux.auth.service;
+
+import com.xiyoulinux.common.JwtToken;
+import com.xiyoulinux.common.UsernameAndPassword;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+/**
+ * @author qkm
+ */
+public interface IAuthService {
+
+    /**
+     * 从授权中心获取 Token (登录功能), 且返回信息中没有统一响应的包装
+     */
+    JwtToken login(UsernameAndPassword usernameAndPassword) throws NoSuchAlgorithmException, InvalidKeySpecException;
+
+
+    /**
+     * 注册用户并返回当前注册用户的 Token, 即通过授权中心创建用户
+     */
+    JwtToken register(UsernameAndPassword usernameAndPassword) throws NoSuchAlgorithmException, InvalidKeySpecException;
+
+}
