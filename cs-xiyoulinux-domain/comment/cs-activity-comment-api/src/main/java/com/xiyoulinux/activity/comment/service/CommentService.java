c23882725f611b3f69f7bef8b1a6c30102275448
QKM-0203 <com_qkm@xiyoulinux.org>
2021-10-20 20:08:12 +0800

    feat(activity-comment):add business api

---
+++ cs-xiyoulinux-domain/comment/cs-activity-comment-api/src/main/java/com/xiyoulinux/activity/comment/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.xiyoulinux.activity.comment.service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author qkm
+ */
+public interface CommentService {
+    /**
+     * 获取当前动态的评论数
+     *
+     * @param activityIdList 动态id
+     * @return 评论数目
+     */
+    Map<String, Long> getCommentNumber(List<String> activityIdList);
+
+    /**
+     * 删除当前动态的所有评论信息
+     *
+     * @param activityId 动态id
+     */
+    void deleteComments(String activityId);
+}
