98f86c1edbc7e0677ec1b28d2937ccc04340958b
zhoukuo123 <1178053078@qq.com>
2021-10-23 20:17:11 +0800

    feat(join-admin): add mapper and mapper.xml

--- cs-xiyoulinux-domain/join_admin/cs-join-admin-mapper/src/main/java/com/xiyoulinux/joinadmin/mapper/JoinRecordMapperCustom.java
+++ cs-xiyoulinux-domain/join_admin/cs-join-admin-mapper/src/main/java/com/xiyoulinux/joinadmin/mapper/JoinRecordMapperCustom.java
@@ -3,6 +3,7 @@ package com.xiyoulinux.joinadmin.mapper;
 import com.xiyoulinux.joinadmin.pojo.JoinRecord;
 import com.xiyoulinux.joinadmin.pojo.vo.InterviewEvaluationRecordVO;
 import com.xiyoulinux.joinadmin.pojo.vo.InterviewQueueVO;
+import com.xiyoulinux.joinadmin.pojo.vo.IntervieweeInfoAndGradeVO;
 import com.xiyoulinux.my.mapper.MyMapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -17,6 +18,14 @@ public interface JoinRecordMapperCustom extends MyMapper<JoinRecord> {
      * @return 面试评价记录List
      */
     List<InterviewEvaluationRecordVO> queryInterviewEvaluationRecord(@Param("paramsMap") Map<String, Object> map);
+
+    /**
+     * 根据 round 和 status 查询面试信息和被面试人信息 用于决策
+     *
+     * @param map map.round  map.status
+     * @return 面试评价记录List
+     */
+    List<IntervieweeInfoAndGradeVO> queryInterviewInfoAndIntervieweeInfo(@Param("paramsMap") Map<String, Object> map);
 
 
 }
