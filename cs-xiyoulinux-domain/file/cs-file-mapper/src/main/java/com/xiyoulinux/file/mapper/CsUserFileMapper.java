60a56fdaefc97efe0c19ea8dfe7b7167dde1cab1
QKM-0203 <com_qkm@xiyoulinux.org>
2021-10-25 20:17:04 +0800

    feat(file):add mapper and mapper.xml

---
+++ cs-xiyoulinux-domain/file/cs-file-mapper/src/main/java/com/xiyoulinux/file/mapper/CsUserFileMapper.java
@@ -0,0 +1,17 @@
+package com.xiyoulinux.file.mapper;
+
+import com.xiyoulinux.file.entity.CsUserFile;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author qkm
+ * @Entity com.xiyoulinux.file.pojo.CsUserFile
+ */
+@Repository
+public interface CsUserFileMapper extends BaseMapper<CsUserFile> {
+}
+
+
+
+
