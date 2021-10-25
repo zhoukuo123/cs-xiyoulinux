c2d889d9f46e0a226b09d4a45ee90996dccbef6d
QKM-0203 <com_qkm@xiyoulinux.org>
2021-10-25 20:17:04 +0800

    feat(file):add mapper and mapper.xml

---
+++ cs-xiyoulinux-domain/file/cs-file-mapper/src/main/java/com/xiyoulinux/file/mapper/CsActivityFileMapper.java
@@ -0,0 +1,36 @@
+package com.xiyoulinux.file.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xiyoulinux.file.entity.CsActivityFile;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+/**
+ * @author qkm
+ * @Entity com.xiyoulinux.file.pojo.CsActivityFile
+ */
+@Repository
+public interface CsActivityFileMapper extends BaseMapper<CsActivityFile> {
+
+    /**
+     * 根据动态id获取对应的文件
+     *
+     * @param activityIdList 动态idList
+     * @return 文件集合
+     */
+    List<CsActivityFile> selectAllByActivityIdList(@Param("activityIdList") List<String> activityIdList);
+
+    /**
+     * 删除动态对应的文件信息
+     *
+     * @param activityId 动态id
+     */
+    void delActivityFileByActivityId(@Param("activityId") String activityId);
+}
+
+
+
+
