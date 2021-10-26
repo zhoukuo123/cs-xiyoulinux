3c7e419b98706b737f666b1aaba2e53952289618
zhoukuo123 <1178053078@qq.com>
2021-10-26 18:02:25 +0800

    feat(join-admin): add message queue dto (batch decision)

--- cs-xiyoulinux-domain/join_admin/cs-join-admin-pojo/src/main/java/com/xiyoulinux/joinadmin/pojo/dto/BatchDecisionDTO.java
+++ cs-xiyoulinux-domain/join_admin/cs-join-admin-pojo/src/main/java/com/xiyoulinux/joinadmin/pojo/dto/BatchDecisionDTO.java
@@ -8,29 +8,27 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author CoderZk
  */
-@ApiModel(value = "纳新设置DTO", description = "纳新设置DTO")
+@ApiModel(value = "批量决策DTO", description = "用于批量决策发送消息到MQ")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class JoinSettingDTO implements Serializable {
+public class BatchDecisionDTO implements Serializable {
+
+    private List<String> uids;
 
     /**
-     * 纳新报名开始时间
-     */
-    private Date joinStartTime;
-
-    /**
-     * 纳新报名截止时间
-     */
-    private Date joinEndTime;
-
-    /**
-     * 本次纳新有多少轮
+     * 第几轮面试
      */
     private Integer round;
+
+    /**
+     * 是否通过
+     */
+    private boolean pass;
 }
