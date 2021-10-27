41ebea1ffb146235ae12af371879e1f6cb6339b4
QKM-0203 <com_qkm@xiyoulinux.org>
2021-10-27 20:22:13 +0800

    feat(gateway):add global intercept

---
+++ cs-xiyoulinux-platform/gateway/src/main/java/com/xiyoulinux/constant/GlobalConstant.java
@@ -0,0 +1,18 @@
+package com.xiyoulinux.constant;
+
+
+/**
+ * @author qkm
+ */
+public class GlobalConstant {
+    /**
+     * 登录 uri
+     */
+    public static final String USER_LOGIN_PATH = "/cs-auth/authority/token";
+
+    /**
+     * 注册 uri
+     */
+    public static final String USER_REGISTER_PATH = "/cs-auth/authority/register";
+
+}
