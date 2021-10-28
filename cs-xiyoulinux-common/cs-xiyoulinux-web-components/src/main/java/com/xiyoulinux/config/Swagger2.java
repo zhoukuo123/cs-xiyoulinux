1a049c7b2fa9a6d5192c09bbf63b6185397b5c3b
QKM-0203 <com_qkm@xiyoulinux.org>
2021-10-28 20:28:15 +0800

    feat(web):modify swagger2 config

--- cs-xiyoulinux-common/cs-xiyoulinux-web-components/src/main/java/com/xiyoulinux/config/Swagger2.java
+++ cs-xiyoulinux-common/cs-xiyoulinux-web-components/src/main/java/com/xiyoulinux/config/Swagger2.java
@@ -2,15 +2,20 @@ package com.xiyoulinux.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author CoderZk
@@ -22,20 +27,25 @@ public class Swagger2 {
     // http://localhost:8088/doc.html               引入的依赖皮肤路径
 
 
-    // 配置swagger2核心配置 docket
     @Bean
     public Docket createRestApi() {
+//        List<Parameter> pars = new ArrayList<>();
+//        ParameterBuilder ticketPar = new ParameterBuilder();
+//        ticketPar.name("cs-user").description("user ticket")
+//
+//                .modelRef(new ModelRef("string")).parameterType("header")
+//
+//                .required(true).build(); //header中的ticket参数非必填，传空也可以
+
+//        pars.add(ticketPar.build()); //根据每个方法名也知道当前方法在设置什么参数
+
         return new Docket(DocumentationType.SWAGGER_2)  // 指定api类型为swagger2
                 .apiInfo(apiInfo())                     // 用于定义api文档汇总信息
-                .select()
-                .apis(RequestHandlerSelectors
-                        .withClassAnnotation(RestController.class) // 指定Service上的RestController注解
-//                        .basePackage("com.zk.controller")) // 指定controller包
-                )
+                .select().apis(RequestHandlerSelectors.basePackage("com.xiyoulinux")) // 指定controller包
                 .paths(PathSelectors.any())             // 包下的所有controller
                 .build();
+//                .globalOperationParameters(pars);
     }
-
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("CS平台接口api")           // 文档页标题
---
+++ cs-xiyoulinux-common/cs-xiyoulinux-web-components/src/main/java/com/xiyoulinux/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package com.xiyoulinux.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @author CoderZk
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+
+    /**
+     * 让 MVC 加载 Swagger 的静态资源
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/**").
+                addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        super.addResourceHandlers(registry);
+    }
+}
