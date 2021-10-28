8dab8195ef6bdfa56ff70dc222c472b380d2121f
QKM-0203 <com_qkm@xiyoulinux.org>
2021-10-28 20:29:00 +0800

    feat(web):add exception(UnauthorizedException)

---
+++ cs-xiyoulinux-common/cs-xiyoulinux-web-components/src/main/java/com/xiyoulinux/exception/UnauthorizedException.java
@@ -0,0 +1,11 @@
+package com.xiyoulinux.exception;
+
+/**
+ * @author qkm
+ */
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
