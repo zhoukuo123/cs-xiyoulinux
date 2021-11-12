f05abbea94e1d31eefa23317a1b3c7e7b28dc24a
QKM-0203 <com_qkm@xiyoulinux.org>
2021-11-12 09:25:30 +0800

    feat(activity):add fileUrl filed

--- cs-xiyoulinux-domain/activity/cs-activity-web/src/main/resources/sql/activity.sql
+++ cs-xiyoulinux-domain/activity/cs-activity-web/src/main/resources/sql/activity.sql
@@ -4,11 +4,12 @@ create table cs_user_activity
         primary key,
     user_id              mediumtext  not null comment '用户Id',
     activity_title       varchar(70) not null comment '活动标题',
-    activity_content     text        not null comment '问题or动态or讲座or任务的内容',
+    activity_content     varchar(2000) not null comment '问题or动态or讲座or任务的内容',
     activity_end_time    datetime    null     comment  '结束时间',
     activity_status      int         null     comment  '活动状态',
     activity_type        int         not null comment '发起问题or发起动态or发起任务or发起讲座(0/1/2/3)',
-    activity_create_time datetime    not null comment '创建时间'
+    activity_create_time datetime    not null comment '创建时间',
+    activity_files varchar(500) null comment '动态文件信息'
 ) comment '用户动态表' charset = utf8;
 
 create index cs_user_activity_id_activity_type_activity_status_index
@@ -23,7 +24,8 @@ create table cs_user_question
     question_create_time datetime    not null comment '问题创建时间',
     question_status      int         not null comment '问题状态(1, "未解决"、0, "已解决")',
     user_id              varchar(20) not null comment '用户id',
-    question_content     text        not null comment '问题内容',
+    question_content     varchar(2000)  not null comment '问题内容',
+    question_files varchar(500) null comment '问题文件信息'
     constraint cs_user_question_question_id_uindex
         unique (question_id)
 ) comment '用户问题表' charset = utf8;
@@ -41,7 +43,8 @@ create table cs_user_task
     task_create_time datetime    not null comment '任务创建时间',
     task_end_time    datetime    not null comment '任务的结束时间',
     task_status      int         not null comment '任务的状态(2, "进行中"、3, "待进行"、4, "已完成")',
-    task_content     text        not null comment '任务内容',
+    task_content     varchar(2000)        not null comment '任务内容',
+    task_files varchar(500) null comment '任务文件信息'
     constraint cs_user_task_task_id_uindex
         unique (task_id)
 ) comment '用户任务表' charset = utf8;
