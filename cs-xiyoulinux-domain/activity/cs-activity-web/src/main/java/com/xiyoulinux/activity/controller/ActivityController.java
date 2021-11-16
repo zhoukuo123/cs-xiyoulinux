9f5d7191625e9ee3f856276658e66cf896405d34
QKM-0203 <com_qkm@xiyoulinux.org>
2021-11-16 13:56:30 +0800

    feat(activity):delete ApiImplicitParams

--- cs-xiyoulinux-domain/activity/cs-activity-web/src/main/java/com/xiyoulinux/activity/controller/ActivityController.java
+++ cs-xiyoulinux-domain/activity/cs-activity-web/src/main/java/com/xiyoulinux/activity/controller/ActivityController.java
@@ -14,6 +14,7 @@ import com.xiyoulinux.enums.ReturnCode;
 import com.xiyoulinux.pojo.JSONResult;
 import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 
@@ -45,23 +46,20 @@ public class ActivityController {
 //            @ApiImplicitParam(name = "csUserActivityBo", value = "动态的信息", required = true)
 //            @ApiImplicitParam(paramType = "form", name = "files", value = "文件数组", allowMultiple = true,
 //                    dataType = "__file", required = false)
-//            @ApiImplicitParam(name = "files", value = "多个文件", paramType = "formData", allowMultiple = true, required = true, dataType = "file")
+            @ApiImplicitParam(name = "files", value = "多个文件", paramType = "formData", allowMultiple = true, required = true, dataType = "file")
     })
     @ApiResponses({
             @ApiResponse(code = 200, message = "返回用户的信息", response = CsUserInfoAndIdAndFileInfo.class)
     })
-    public JSONResult addActivity(@RequestBody CsUserActivityBo csUserActivityBo
-                                  // @RequestParam(value = "files") MultipartFile[] files
+    public JSONResult addActivity(@RequestBody CsUserActivityBo csUserActivityBo,
+                                  @RequestPart(value = "files") MultipartFile[] files
     ) {
-//        System.out.println(files);
+        System.out.println(files);
         return JSONResult.ok(iCsUserActivityService.addActivity(csUserActivityBo, null));
     }
 
 
     @ApiOperation(value = "删除动态", notes = "根据动态id删除动态", httpMethod = "DELETE")
-    @ApiImplicitParams({
-            @ApiImplicitParam(paramType = "body", name = "csUserActivityDeleteBo", value = "删除动态对象", required = true)
-    })
     @DeleteMapping("/delete")
     @ApiResponses({
             @ApiResponse(code = 200, message = "删除成功")
@@ -73,11 +71,10 @@ public class ActivityController {
 
     @ApiOperation(value = "获取动态", notes = "分页的获取动态数据", httpMethod = "GET")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "userId", value = "用户id", required = true, paramType = "path", dataType = "string"),
-            @ApiImplicitParam(name = "pageInfo", value = "页对象", required = true, paramType = "body")
+            @ApiImplicitParam(name = "userId", value = "用户id", required = true, paramType = "path", dataType = "string")
     })
     @ApiResponses(@ApiResponse(code = 200, message = "动态集合"))
-    @GetMapping("/home/{userId}")
+    @PostMapping("/home/{userId}")
     public JSONResult getPageActivities(@PathVariable("userId") String userId,
                                         @RequestBody PageInfo pageInfo) {
         PageActivityInfo pageActivity = iCsUserActivityService.getPageActivity(pageInfo, userId);
@@ -89,11 +86,10 @@ public class ActivityController {
 
     @ApiOperation(value = "分页的获取未解决的问题", notes = "未解决的问题(1)", httpMethod = "GET")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "userId", value = "用户id", required = true, paramType = "path", dataType = "string"),
-            @ApiImplicitParam(name = "pageInfo", value = "页对象", required = true, paramType = "body")
+            @ApiImplicitParam(name = "userId", value = "用户id", required = true, paramType = "path", dataType = "string")
     })
     @ApiResponses(@ApiResponse(code = 200, message = "未解决问题集合"))
-    @GetMapping("/issues/unsolved/{userId}")
+    @PostMapping("/issues/unsolved/{userId}")
     public JSONResult getUnresolvedIssues(@PathVariable("userId") String userId,
                                           @RequestBody PageInfo pageInfo) {
         PageQuestionInfo pageUnresolvedIssues = iCsUserQuestionService.getPageUnresolvedIssues(pageInfo, userId);
@@ -106,11 +102,10 @@ public class ActivityController {
 
     @ApiOperation(value = "分页的获取已解决的问题", notes = "已解决的问题(0)", httpMethod = "GET")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "userId", value = "用户id", required = true, paramType = "path", dataType = "string"),
-            @ApiImplicitParam(name = "pageInfo", value = "页对象", required = true, paramType = "body")
+            @ApiImplicitParam(name = "userId", value = "用户id", required = true, paramType = "path", dataType = "string")
     })
     @ApiResponses(@ApiResponse(code = 200, message = "已解决问题集合"))
-    @GetMapping("/issues/solved/{userId}")
+    @PostMapping("/issues/solved/{userId}")
     public JSONResult getResolvedIssues(@PathVariable("userId") String userId,
                                         @RequestBody PageInfo pageInfo) {
         PageQuestionInfo pageResolvedIssues = iCsUserQuestionService.getPageResolvedIssues(pageInfo, userId);
@@ -123,11 +118,10 @@ public class ActivityController {
 
     @ApiOperation(value = "分页的获取进行中的任务", notes = "进行中的任务(2)", httpMethod = "GET")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "userId", value = "用户id", required = true, paramType = "path", dataType = "string"),
-            @ApiImplicitParam(name = "pageInfo", value = "页对象", required = true, paramType = "body")
+            @ApiImplicitParam(name = "userId", value = "用户id", required = true, paramType = "path", dataType = "string")
     })
     @ApiResponses(@ApiResponse(code = 200, message = "进行中的任务集合"))
-    @GetMapping("/tasks/doing/{userId}")
+    @PostMapping("/tasks/doing/{userId}")
     public JSONResult getDoingTasks(@PathVariable("userId") String userId,
                                     @RequestBody PageInfo pageInfo) {
         PageTaskInfo pageDoingTasks = iCsUserTaskService.getPageDoingTasks(pageInfo, userId);
@@ -139,11 +133,10 @@ public class ActivityController {
 
     @ApiOperation(value = "分页的获取已完成的任务", notes = "已完成的任务(4)", httpMethod = "GET")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "userId", value = "用户id", required = true, paramType = "path", dataType = "string"),
-            @ApiImplicitParam(name = "pageInfo", value = "页对象", required = true, paramType = "body")
+            @ApiImplicitParam(name = "userId", value = "用户id", required = true, paramType = "path", dataType = "string")
     })
     @ApiResponses(@ApiResponse(code = 200, message = "已完成的任务集合"))
-    @GetMapping("/tasks/did/{userId}")
+    @PostMapping("/tasks/did/{userId}")
     public JSONResult getDidTasks(@PathVariable("userId") String userId,
                                   @RequestBody PageInfo pageInfo) {
         PageTaskInfo pageDidTasks = iCsUserTaskService.getPageDidTasks(pageInfo, userId);
@@ -155,11 +148,10 @@ public class ActivityController {
 
     @ApiOperation(value = "分页的获取待完成的任务", notes = "待完成的任务(3)", httpMethod = "GET")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "userId", value = "用户id", required = true, paramType = "path", dataType = "string"),
-            @ApiImplicitParam(name = "pageInfo", value = "页对象", required = true, paramType = "body")
+            @ApiImplicitParam(name = "userId", value = "用户id", required = true, paramType = "path", dataType = "string")
     })
     @ApiResponses(@ApiResponse(code = 200, message = "待进行的任务集合"))
-    @GetMapping("/tasks/future/{userId}")
+    @PostMapping("/tasks/future/{userId}")
     public JSONResult getFutureTasks(@PathVariable("userId") String userId,
                                      @RequestBody PageInfo pageInfo) {
         PageTaskInfo pageFutureTasks = iCsUserTaskService.getPageFutureTasks(pageInfo, userId);
@@ -170,9 +162,6 @@ public class ActivityController {
     }
 
     @ApiOperation(value = "修改任务", notes = "修改任务对象的内容", httpMethod = "PUT")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "csUserTaskUpdateBo", value = "更新任务的信息", required = true)
-    })
     @ApiResponses(@ApiResponse(code = 200, message = "更新成功"))
     @PutMapping("/modify/task")
     public JSONResult updateTask(@RequestBody CsUserTaskUpdateBo csUserTaskUpdateBo) {
@@ -181,9 +170,6 @@ public class ActivityController {
     }
 
     @ApiOperation(value = "修改问题", notes = "修改问题的状态", httpMethod = "PUT")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "csUserQuestionUpdateBo", value = "更新问题的信息", required = true)
-    })
     @ApiResponses(@ApiResponse(code = 200, message = "更新成功"))
     @PutMapping("/modify/question")
     public JSONResult updateQuestion(@RequestBody CsUserQuestionUpdateBo csUserQuestionUpdateBo) {
