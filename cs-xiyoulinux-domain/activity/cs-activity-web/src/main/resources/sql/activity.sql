d6b5c325d6e1902f3ef626661afb1fe3c20800c9
QKM-0203 <com_qkm@xiyoulinux.org>
2021-11-18 22:23:31 +0800

    feat(activity):modify activity.sql

--- cs-xiyoulinux-domain/activity/cs-activity-web/src/main/resources/sql/activity.sql
+++ cs-xiyoulinux-domain/activity/cs-activity-web/src/main/resources/sql/activity.sql
@@ -1,53 +1,57 @@
 create table cs_user_activity
 (
-    id                   varchar(20) not null comment '主键'
+    id                   varchar(20)   not null comment '主键'
         primary key,
-    user_id              mediumtext  not null comment '用户Id',
-    activity_title       varchar(70) not null comment '活动标题',
+    user_id              mediumtext    not null comment '用户Id',
+    activity_title       varchar(70)   not null comment '活动标题',
     activity_content     varchar(2000) not null comment '问题or动态or讲座or任务的内容',
-    activity_end_time    datetime    null     comment  '结束时间',
-    activity_status      int         null     comment  '活动状态',
-    activity_type        int         not null comment '发起问题or发起动态or发起任务or发起讲座(0/1/2/3)',
-    activity_create_time datetime    not null comment '创建时间',
-    activity_files varchar(500) null comment '动态文件信息'
-) comment '用户动态表' charset = utf8;
+    activity_end_time    datetime      null comment '结束时间',
+    activity_status      int           null,
+    activity_type        int           not null comment '问题or动态or讲座or任务(0/1/2/3)',
+    activity_create_time datetime      not null comment '创建时间',
+    activity_files       varchar(500)  null comment '动态文件url以逗号分割',
+    activity_update_time datetime    not null ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP comment '更新时间',
+    activity_update_time datetime      null comment '更新时间'
+)
+    comment '用户动态表' charset = utf8;
 
 create index cs_user_activity_id_activity_type_activity_status_index
     on cs_user_activity (id, activity_type, activity_status);
 
 create table cs_user_question
 (
-    id                   varchar(20) not null comment '主键id'
+    id                   varchar(20)   not null comment '主键id'
         primary key,
-    question_id          varchar(20) not null comment '问题id--同动态表中的id',
-    question_title       varchar(70) not null comment '问题标题',
-    question_create_time datetime    not null comment '问题创建时间',
-    question_status      int         not null comment '问题状态(1, "未解决"、0, "已解决")',
-    user_id              varchar(20) not null comment '用户id',
-    question_content     varchar(2000)  not null comment '问题内容',
-    question_files varchar(500) null comment '问题文件信息'
+    question_id          varchar(20)   not null comment '问题id--同动态表中的id',
+    question_title       varchar(70)   not null comment '问题标题',
+    question_create_time datetime      not null comment '问题创建时间',
+    question_status      int           not null comment '问题是否解决-0是未解决/1是解决',
+    user_id              varchar(20)   not null comment '用户id',
+    question_content     varchar(2000) not null comment '问题内容',
+    question_files       varchar(500)  null comment '问题文件url以逗号分割',
     constraint cs_user_question_question_id_uindex
         unique (question_id)
-) comment '用户问题表' charset = utf8;
+)
+    comment '用户问题表';
 
 create index cs_user_question_question_id_question_status_index
     on cs_user_question (question_id, question_status);
 
 create table cs_user_task
 (
-    id               varchar(20) not null comment '主键id'
+    id               varchar(20)   not null comment '主键id'
         primary key,
-    user_id          varchar(20) not null comment '用户id',
-    task_id          varchar(20) not null comment '任务id--同动态表的id',
-    task_title       varchar(70) not null comment '任务标题',
-    task_create_time datetime    not null comment '任务创建时间',
-    task_end_time    datetime    not null comment '任务的结束时间',
-    task_status      int         not null comment '任务的状态(2, "进行中"、3, "待进行"、4, "已完成")',
-    task_content     varchar(2000)        not null comment '任务内容',
-    task_files varchar(500) null comment '任务文件信息'
+    user_id          varchar(20)   not null comment '用户id',
+    task_id          varchar(20)   not null comment '任务id--同动态表的id',
+    task_title       varchar(70)   not null comment '任务标题',
+    task_create_time datetime      not null comment '任务创建时间',
+    task_end_time    datetime      not null comment '任务的结束时间',
+    task_status      int           not null comment '任务的状态',
+    task_content     varchar(2000) not null comment '任务内容',
+    task_files       varchar(500)  null comment '任务文件url以逗号分割',
     constraint cs_user_task_task_id_uindex
         unique (task_id)
-) comment '用户任务表' charset = utf8;
+);
 
 create table undo_log
 (
@@ -63,4 +67,6 @@ create table undo_log
     ext           varchar(100) null,
     constraint ux_undo_log
         unique (xid, branch_id)
-) comment 'seata need' charset = utf8;
+)
+    charset = utf8;
+
