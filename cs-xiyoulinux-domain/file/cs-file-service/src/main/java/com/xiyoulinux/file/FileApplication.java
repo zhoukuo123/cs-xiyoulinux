7be5eafb4dcd6e68e0c2b3785b18a1070114b12a
QKM-0203 <com_qkm@xiyoulinux.org>
2021-11-20 09:26:09 +0800

    feat(file):modify package name

--- cs-xiyoulinux-domain/file/cs-file-service/src/main/java/com/xiyoulinux/file/FileApplication.java
+++ cs-xiyoulinux-domain/file/cs-file-service/src/main/java/com/xiyoulinux/file/FileApplication.java
@@ -1,6 +1,5 @@
 package com.xiyoulinux.file;
 import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author qkm
  */
 @SpringBootApplication(scanBasePackages = {"com.xiyoulinux","org.n3r.idworker"})
-@MapperScan(basePackages = "com.xiyoulinux.file.mapper")
+@DubboComponentScan(basePackages = "com.xiyoulinux.file.impl")
 @EnableDiscoveryClient
 public class FileApplication {
     public static void main(String[] args) {
